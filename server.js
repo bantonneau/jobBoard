751e3ad1b5af326a09ca0f6cbebcd44681d7ebc1
ToriKnix <toriknixmusic@gmail.com>
2023-06-07 19:59:48 -0600

    dotENV

---
+++ server.js
@@ -0,0 +1,47 @@
+const path = require('path');
+const express = require('express');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const routes = require('./controllers');
+const helpers = require('./utils/helpers');
+
+
+const sequelize = require('../../14-MVC/01-Activities/24-Stu_Auth-Review/Unsolved/config/connection');
+
+// TODO: Add a comment describing the functionality of this expression
+// This isis creating sessions storage so you are able to grab cookies
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const hbs = exphbs.create({ helpers });
+
+// TODO: Add a comment describing the functionality of this object
+// This is everything that is going to be stored in a session
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+// TODO: Add a comment describing the functionality of this statement
+// This is initializing the middleware
+app.use(session(sess));
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
+});
