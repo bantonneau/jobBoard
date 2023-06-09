f0fc56f680c84c232c65e113fa6e2cefb20d3137
ToriKnix <toriknixmusic@gmail.com>
2023-06-09 09:11:01 -0600

    No longer using Json Web Token. Solely bcrypt.

--- Controllers/authControllers.js
+++ Controllers/authControllers.js
@@ -1,6 +1,4 @@
-// controllers/authController.js
 const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
 const { User } = require('../models');
 
 const authController = {
@@ -11,7 +9,7 @@ const authController = {
       const user = await User.findOne({ where: { username } });
 
       if (!user) {
-        return res.status(401).json({ error: 'Invalid credentials' });
+        return res.status(401).json({ error: 'Invalid Login' });
       }
 
       const passwordMatch = await bcrypt.compare(password, user.password);
@@ -19,22 +17,17 @@ const authController = {
         return res.status(401).json({ error: 'Invalid credentials' });
       }
 
-      const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET, {
-        expiresIn: '1h',
-      });
-
-      res.cookie('token', token, { httpOnly: true });
       res.json({ message: 'Login successful' });
     } catch (error) {
       console.error(error);
-      res.status(500).json({ error: 'Internal server error' });
+      res.status(500).json({ error: 'There was a problem with the server' });
     }
   },
 
   logout: (req, res) => {
-    res.clearCookie('token');
     res.json({ message: 'Logout successful' });
   },
 };
 
 module.exports = authController;
+
